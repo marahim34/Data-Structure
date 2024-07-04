4e85f1f6fdff6af063a231697d75f708dc670a82
Md Abdur Rahim <98558957+marahim34@users.noreply.github.com>
2024-07-04 14:42:01 +0300

    practice

---
+++ Module-11/design_browser_history.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int sum(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+
+    return 0;
+}
---
+++ Module-11/index.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+int sz = 0;
+
+void insert_at_tail(Node *&head, int val)
+{
+    sz++;
+    Node *newNode = new Node(val);
+
+    if (head == NULL)
+    {
+        head = newNode;
+        return;
+    }
+
+    Node *tmp = head;
+    while (tmp->next != NULL)
+    {
+        tmp = tmp->next;
+        }
+
+    tmp->next = newNode;
+    newNode->prev = tmp;
+}
+
+void print_list(Node *head)
+{
+    Node *tmp = head;
+
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+
+void size(Node *head)
+{
+    Node *tmp = head;
+    int count = 0;
+    while (tmp != NULL)
+    {
+        count++;
+        tmp = tmp->next;
+    }
+    cout << count << endl;
+}
+
+void print_orderOne(Node *head)
+{
+    cout << sz << endl;
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int val;
+    while (true)
+    {
+        cin >> val;
+
+        if (val == -1)
+        {
+            break;
+        }
+
+        insert_at_tail(head, val);
+    }
+
+    size(head);
+
+    print_list(head);
+
+    print_orderOne(head);
+
+    return 0;
+}
