3051b1a4a38da9efd4f4568a8ad36e1cbc63b7a8
Md Abdur Rahim <98558957+marahim34@users.noreply.github.com>
2024-07-08 00:00:07 +0300

    stack input

--- Module-13/stack_using_array.cpp
+++ Module-13/stack_using_array.cpp
@@ -33,6 +33,34 @@ public:
 int main()
 {
     myStack st;
+    // st.push(10);
+    // st.push(20);
+    // st.push(30);
+    // cout << st.top() << endl;
+    // st.pop();
+    // cout << st.top() << endl;
+    // st.pop();
+    // cout << st.top() << endl;
+
+    // if (st.empty() == false)
+    // {
+    //     cout << st.top() << endl;
+    // }
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        st.push(x);
+    }
+
+    while (st.empty() == false)
+    {
+        cout << st.top() << endl;
+        st.pop();
+    }
 
     return 0;
 }
