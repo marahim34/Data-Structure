7278abdd79eb6ec7ba81036bed1d895c7bf15ffc
Md Abdur Rahim <98558957+marahim34@users.noreply.github.com>
2024-07-09 00:45:12 +0300

    queue with doubly linked list

---
+++ Module-14/queue_using_doubly_Linked_list.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+class myQueue
+{
+public:
+    Node *head = NULL;
+    Node *tail = NULL;
+    int sz = 0;
+
+    void push(int val)
+    {
+        sz++;
+        Node *newNode = new Node(val);
+
+        if (!head)
+        {
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        else
+        {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = tail->next;
+        }
+    }
+
+    void pop()
+    {
+        sz--;
+        Node *deleteNode = head;
+        head = head->next;
+        if (!head)
+        {
+            tail = NULL;
+            delete deleteNode;
+            return;
+        }
+        head->prev = NULL;
+        delete deleteNode;
+    }
+
+    int front()
+    {
+        return head->val;
+    }
+
+    int size()
+    {
+        return sz;
+    }
+
+    bool empty()
+    {
+        if (!sz)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
+
+int main()
+{
+    myQueue q;
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        q.push(x);
+    }
+
+    while (!q.empty())
+    {
+        cout << q.front() << " ";
+        q.pop();
+    }
+
+    q.push(10);
+
+    return 0;
+}
