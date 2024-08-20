4d3afb454eb89288d612f34a7915566bf94fb599
Md Abdur Rahim <98558957+marahim34@users.noreply.github.com>
2024-08-20 09:18:42 +0300

    level traversing

---
+++ Module-18/input_binary_tree.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node *input_tree()
+{
+    int val;
+    cin >> val;
+    Node *root;
+
+    if (val == -1)
+        root = NULL;
+    else
+        root = new Node(val);
+
+    queue<Node *> q;
+    if (root)
+        q.push(root);
+
+    while (!q.empty())
+    {
+        // 1. bring them from line
+        Node *p = q.front();
+        q.pop();
+
+        // 2. Do all necessary
+        int l, r;
+        cin >> l >> r;
+
+        Node *left;
+        Node *right;
+        if (l == -1)
+            left = NULL;
+        else
+            left = new Node(l);
+
+        if (r == -1)
+            right = NULL;
+        else
+            right = new Node(r);
+
+        p->left = left;
+        p->right = right;
+
+        // 3. bring all children from the lines
+        if (p->left)
+            q.push(p->left);
+        if (p->right)
+            q.push(p->right);
+    }
+    return root;
+}
+
+void level_order(Node *root)
+{
+    if (root == NULL)
+    {
+        cout << "Tree Nai" << endl;
+        return;
+    }
+
+    queue<Node *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        // bring them from line
+        Node *f = q.front();
+        q.pop();
+
+        // do all necessary works
+        cout << f->val << " ";
+
+        // Keep the children from the line
+        if (f->left != NULL)
+        {
+            q.push(f->left);
+        }
+        if (f->right != NULL)
+        {
+            q.push(f->right);
+        }
+    }
+}
+
+int main()
+{
+    Node *root = input_tree();
+
+    level_order(root);
+    return 0;
+}
