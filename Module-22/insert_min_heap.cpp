c9b8a39b041c0120c9611d4b4cae9b7a3ec97e9a
Md Abdur Rahim <98558957+marahim34@users.noreply.github.com>
2024-08-26 14:45:54 +0300

    max heap and min heap

---
+++ Module-22/insert_min_heap.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> v;
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+
+        v.push_back(x);
+
+        int cur_ind = v.size() - 1;
+        // cout << cur_ind << endl;
+
+        // cout << parent << endl;
+
+        while (cur_ind != 0)
+        {
+            int parent = (cur_ind - 1) / 2;
+            if (v[parent] > v[cur_ind])
+                swap(v[parent], v[cur_ind]);
+            else
+                break;
+
+            cur_ind = parent;
+        }
+    }
+
+    for (int val : v)
+    {
+        cout << val << " ";
+    }
+
+    return 0;
+}
